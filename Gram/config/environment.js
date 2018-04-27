affd4d0ad80a58cc348088ed50144e34ecf375d2
Usman Balesaria <ubalesaria@me.com>
2018-04-27 09:58:25 +0100

    Preparing for deployment

--- Gram/config/environment.js
+++ Gram/config/environment.js
@@ -1,4 +1,4 @@
-const databaseURI = 'mongodb://localhost/gram';
-const port = 3000;
+const databaseURI = process.env.MONGODB_URI || 'mongodb://localhost/gram';
+const port = process.env.PORT || 3000;
 
 module.exports ={databaseURI, port};
